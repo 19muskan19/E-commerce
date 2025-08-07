2957f50294de58defe72702ce2fae1daea381a16
19muskan19 <muskanpatel505@gmail.com>
2025-08-07 15:31:41 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import{createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // import './index.css'
 import App from './App.jsx'
 import MyShop from './Github/MyShop/MyShop.jsx'
